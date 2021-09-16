b8a1f7338e80de9c9f2f3782fcf6f04910e9b0a1
mosamircs <mosamircs@gmail.com>
2021-09-16 14:20:18 +0200

    add nav bar

--- js/app.js
+++ js/app.js
@@ -18,15 +18,34 @@
  * 
 */
 
+const navBarMake=()=>{
+    //add nav bar sections as links to each section  
+    var section=1;
+    while(section<5){
+        //create nav bar element
+        var node = document.createElement("a");   
+        //the text inside each nav bar element         
+        var textnode = document.createTextNode(`section${section}`);
+        //style nav bar as white back ground
+        node.style.backgroundColor = "white";
+        //remove underline form anchor
+
+        node.appendChild(textnode);
+        //link every nav bar element to specific section
+        node.href = `#section${section}`; 
+        //append each nav bar element to unoreder list (nav bar )
+        document.getElementById("navbar__list").appendChild(node);
+        section++;
+    }
+
+}
+navBarMake();
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
-
-
 /**
  * End Helper Functions
  * Begin Main Functions
