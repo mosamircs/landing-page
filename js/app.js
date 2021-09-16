0f7f44301a88570a783e3261dc35f9a09077ead7
mosamircs <mosamircs@gmail.com>
2021-09-16 18:50:48 +0200

    case on click changing color of nav bar element

--- js/app.js
+++ js/app.js
@@ -1,76 +1,47 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-
-const navBarMake=()=>{
-    //add nav bar sections as links to each section  
+function navBarMake(){
+    //add nav bar sections as links to each section
     var section=1;
     while(section<5){
         //create nav bar element
-        var node = document.createElement("a");   
-        //the text inside each nav bar element         
+        var node = document.createElement("a");
+        //the text inside each nav bar element
         var textnode = document.createTextNode(`section${section}`);
         //style nav bar as white back ground
         node.style.backgroundColor = "white";
         //remove underline form anchor
-
+        node.style.textDecoration = "none";
+        //styling elements
+        node.style.paddingRight="25";
+        node.style.paddingBottom="10";
+        node.style.paddingTop="20";
         node.appendChild(textnode);
         //link every nav bar element to specific section
-        node.href = `#section${section}`; 
+        node.href = `#section${section}`;
         //append each nav bar element to unoreder list (nav bar )
         document.getElementById("navbar__list").appendChild(node);
+
+        node.setAttribute("id", `sec${section}`);
+        //case on click changing color of nav bar element
+        if(document.getElementById(`sec${section}`)!=""){
+            document.getElementById(`sec${section}`).addEventListener("click",function(){
+                this.style.backgroundColor="black";
+                this.style.color="white";
+            })
+        }
+
+        // nodesection=1;
+        // while (nodesection<5) {
+        //   var nodes=document.getElementById(`sec${section}`);
+        //   node.addEventListener("click",function(){
+        //     nodes.style.backgroundColor="black";
+        //   })
+        //
+        //   nodesection++;
+        // }
+
         section++;
     }
 
+
 }
 navBarMake();
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
