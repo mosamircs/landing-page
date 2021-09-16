1da88bbc3b15b34e53452b4468fe6d401bfb272d
mosamircs <mosamircs@gmail.com>
2021-09-16 19:20:17 +0200

    final the task

--- js/app.js
+++ js/app.js
@@ -12,8 +12,8 @@ function navBarMake(){
         node.style.textDecoration = "none";
         //styling elements
         node.style.paddingRight="25";
-        node.style.paddingBottom="10";
-        node.style.paddingTop="20";
+        // node.style.paddingBottom="10";
+        // node.style.paddingTop="20";
         node.appendChild(textnode);
         //link every nav bar element to specific section
         node.href = `#section${section}`;
@@ -21,12 +21,32 @@ function navBarMake(){
         document.getElementById("navbar__list").appendChild(node);
 
         node.setAttribute("id", `sec${section}`);
-        //case on click changing color of nav bar element
+        //case on click changing color of nav
         if(document.getElementById(`sec${section}`)!=""){
             document.getElementById(`sec${section}`).addEventListener("click",function(){
                 this.style.backgroundColor="black";
                 this.style.color="white";
-            })
+              
+                this.addEventListener("mouseout", event => {
+                    this.style.backgroundColor="white";
+                    this.style.color="black";
+                });
+            }) 
+
+
+        }
+        const outsideClickListener = event => {
+            const deltaX = event.offsetX - lastMouseDownX
+            const deltaY = event.offsetY - lastMouseDownY
+            const distSq = (deltaX * deltaX) + (deltaY * deltaY)
+            const isDrag = distSq > 3
+            const isDragException = isDrag && !lastMouseDownWasOutside
+    
+            if (!element.contains(event.target) && isVisible(element) && !isDragException) { // or use: event.target.closest(selector) === null
+              element.style.display = 'none'
+              removeClickListener()
+              document.removeEventListener('mousedown', mouseDownListener); // Or add this line to removeClickListener()
+            }
         }
 
         // nodesection=1;
