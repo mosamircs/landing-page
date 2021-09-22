1ebff68de03ef47588851069a9708fd7c132d594
mosamircs <mosamircs@gmail.com>
2021-09-23 00:22:43 +0200

    the final work for the landing page

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
+
+
+/**function navBarMake work: 
+ * 
+ * this function creates the nav bar 
+ * and gives it the styles 
+ */
 function navBarMake(){
-    var section=1;
-    while(section<5) {
-        var a=document.createElement("a");
-        a.href = `#section${section}`;
-        var textnode = document.createTextNode(`Section${section}`);
-        var li = document.createElement("li");
-        li.appendChild(a);
-        document.getElementById('navbar__list').appendChild(li);
-        a.style.backgroundColor = "white";
-        a.style.fontFamily="serif";
-        a.style.fontSize="25";
-        a.style.textDecoration = "none";
-        a.style.paddingRight = "25";
-        a.appendChild(textnode);
-        li.setAttribute("id", `sec${section}`);
-        if (document.getElementById(`sec${section}`) != "") {
-            document.getElementById(`sec${section}`).addEventListener("click", function () {
+    const sectionsList =document.querySelectorAll("section");//to get number of sections
+    var sectionCounter=1;//start count for while loop
+    let sectionsLength=sectionsList.length;//number of sections
+    while(sectionCounter<=sectionsLength) {
+        var link=document.createElement("a");//created to be in list of navbar
+        link.href = `#section${sectionCounter}`;//the link in the navbar refer to specific section
+        link.className="menu__link";    
+        var navBarText = document.createTextNode(`Section ${sectionCounter}`);//text inside each navbar element 
+        var navBarItem = document.createElement("li");
+        navBarItem.appendChild(link);//adds each link to list element  unordered list
+        document.getElementById('navbar__list').appendChild(navBarItem);//adds linkelement  to navbar unordered list
+        link.appendChild(navBarText);
+        navBarItem.setAttribute("id", `sec${sectionCounter}`);
+        if (document.getElementById(`sec${sectionCounter}`) != "") {
+            document.getElementById(`sec${sectionCounter}`).addEventListener("click", function () {
                 this.style.backgroundColor = "black";
                 this.style.color = "white";
                 this.addEventListener("mouseout", event => {
@@ -23,8 +28,28 @@ function navBarMake(){
                     this.style.color = "black";
                 });
             })
-            section++;
+            sectionCounter++;
         }
-    }
+    } 
 }
+
+/**chooseActiveClass
+ * focus on active class in viewport which you are in 
+ */
+function viewPort(section){
+    let sectionPosition=section.getBoundingClientRect();
+    return (sectionPosition.top>=0);
+}
+//creates the nav bar
 navBarMake();
+//add class "active" to section when near to viewport
+document.addEventListener("scroll",document.querySelectorAll('section').forEach(section => {   
+        //checks if the section in viewport and it d
+        if(viewPort(section)){
+            if(!section.classList.contains('your-active-class')){
+                section.classList.add("your-active-class");
+            }else{
+                section.classList.remove("your-active-class");
+            }
+}
+}));
